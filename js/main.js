b2289bc1f063fdd3c3b56cb914d851ca6e81fda0
Эмиль Абусев <emilabusev@yandex.ru>
2023-07-25 17:00:06 +0300

    Выполнение домашнего задания. Добавляет валидацию.

--- js/main.js
+++ js/main.js
@@ -1,5 +1,12 @@
+import './form-picture.js';
+
+
 import {createPictures} from './data.js';
 import {renderMiniature} from './miniatures.js';
+import {onFormSubmit} from './validate.js';
+
 
 renderMiniature(createPictures());
+onFormSubmit();
+
 
--- js/util.js
+++ js/util.js
@@ -1,6 +1,14 @@
+const Keys = {
+  ESCAPE: 'Escape',
+  ESC: 'Esc'
+};
+
 const getRandomInRange = (min, max) => Math.floor(Math.random() * (max - min + 1)) + min;
 
-const onEscKeyDown = (evt) => evt.key === 'Escape';
+const onEscKeyDown = (evt) => evt.key === Keys.ESCAPE || evt.key === Keys.ESC;
+
+const normalizeString = (str) => str.toLowerCase().trim();
 
 export { getRandomInRange };
 export { onEscKeyDown };
+export { normalizeString };
