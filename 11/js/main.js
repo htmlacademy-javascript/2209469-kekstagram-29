aa62ee13cd6910d572d504070cdc90681b904b83
Keks <keks@htmlacademy.ru>
2023-07-27 19:43:45 +0300

    :heavy_check_mark: Сборка #11

---
+++ 11/js/main.js
@@ -0,0 +1,13 @@
+import './form-picture.js';
+import './scale.js';
+
+
+import {createPictures} from './data.js';
+import {renderMiniature} from './miniatures.js';
+import {onFormSubmit} from './validate.js';
+
+
+renderMiniature(createPictures());
+onFormSubmit();
+
+
