323c1265c89e34f2c9aedf70d8ed5ae31afd3118
Эмиль Абусев <emilabusev@yandex.ru>
2023-07-31 19:40:57 +0300

    12.12 Выполнение домашнего задания. Добавляет фильтры.

--- js/util.js
+++ js/util.js
@@ -1,3 +1,5 @@
+const LAG = 500;
+
 const Keys = {
   ESCAPE: 'Escape',
   ESC: 'Esc'
@@ -7,6 +9,27 @@ const onEscKeyDown = (evt) => evt.key === Keys.ESCAPE || evt.key === Keys.ESC;
 
 const normalizeString = (str) => str.toLowerCase().trim();
 
+const debounce = (cb) => {
+  let lastTimeout = null;
+  return (...args) => {
+    if (lastTimeout) {
+      window.clearTimeout(lastTimeout);
+    }
+    lastTimeout = window.setTimeout(() => {
+      cb(...args);
+    }, LAG);
+  };
+};
+
+const shuffleArray = (array) => {
+  for (let indexOne = array.length - 1; indexOne > 0; indexOne--) {
+    const indexTwo = Math.floor(Math.random() * (indexOne + 1));
+    [array[indexOne], array[indexTwo]] = [array[indexTwo], array[indexOne]];
+  }
+  return array;
+};
 
 export { onEscKeyDown };
 export { normalizeString };
+export { debounce };
+export { shuffleArray };
